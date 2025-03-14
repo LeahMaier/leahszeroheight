7e9290753b3be158f2876bef93dcd33bc88bbc6c
LeahMaier <136575273+LeahMaier@users.noreply.github.com>
2025-03-14 16:35:11 +0100

    Create palette.tsx

---
+++ palette.tsx
@@ -0,0 +1,144 @@
+{
+  "almostBlack": {
+    "$type": "color",
+    "$value": "#08090a"
+  },
+  "almostWhite": {
+    "$type": "color",
+    "$value": "#fcfdfd"
+  },
+  "blue": {
+    "100": {
+      "$type": "color",
+      "$value": "#dff0ff"
+    },
+    "300": {
+      "$type": "color",
+      "$value": "#78ceff"
+    },
+    "500": {
+      "$type": "color",
+      "$value": "#16a7f9"
+    },
+    "700": {
+      "$type": "color",
+      "$value": "#0063a7"
+    },
+    "900": {
+      "$type": "color",
+      "$value": "#094671"
+    }
+  },
+  "green": {
+    "100": {
+      "$type": "color",
+      "$value": "#ddfbec"
+    },
+    "300": {
+      "$type": "color",
+      "$value": "#8aebbc"
+    },
+    "500": {
+      "$type": "color",
+      "$value": "#31d385"
+    },
+    "700": {
+      "$type": "color",
+      "$value": "#197c4d"
+    },
+    "900": {
+      "$type": "color",
+      "$value": "#175037"
+    }
+  },
+  "grey": {
+    "100": {
+      "$type": "color",
+      "$value": "#e3e6e9"
+    },
+    "200": {
+      "$type": "color",
+      "$value": "#cfd3d9"
+    },
+    "300": {
+      "$type": "color",
+      "$value": "#acb1bb"
+    },
+    "400": {
+      "$type": "color",
+      "$value": "#878c98"
+    },
+    "50": {
+      "$type": "color",
+      "$value": "#f3f5f7"
+    },
+    "500": {
+      "$type": "color",
+      "$value": "#6c717d"
+    },
+    "600": {
+      "$type": "color",
+      "$value": "#595e69"
+    },
+    "700": {
+      "$type": "color",
+      "$value": "#4b4e57"
+    },
+    "800": {
+      "$type": "color",
+      "$value": "#41434b"
+    },
+    "900": {
+      "$type": "color",
+      "$value": "#393b41"
+    },
+    "950": {
+      "$type": "color",
+      "$value": "#212226"
+    }
+  },
+  "orange": {
+    "100": {
+      "$type": "color",
+      "$value": "#ffefd4"
+    },
+    "300": {
+      "$type": "color",
+      "$value": "#ffc270"
+    },
+    "500": {
+      "$type": "color",
+      "$value": "#ff8a24"
+    },
+    "700": {
+      "$type": "color",
+      "$value": "#c74a07"
+    },
+    "900": {
+      "$type": "color",
+      "$value": "#7f320f"
+    }
+  },
+  "purple": {
+    "100": {
+      "$type": "color",
+      "$value": "#e9e8ff"
+    },
+    "300": {
+      "$type": "color",
+      "$value": "#b7b1ff"
+    },
+    "500": {
+      "$type": "color",
+      "$value": "#775cff"
+    },
+    "700": {
+      "$type": "color",
+      "$value": "#501ee3"
+    },
+    "900": {
+      "$type": "color",
+      "$value": "#37169c"
+    }
+  },
+}
